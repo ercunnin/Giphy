b8708a5372367363fff336e0243615fe84d4f215
The Dude <thedude@Erics-MacBook-Pro.local>
2019-03-11 22:51:08 -0700

    js

---
+++ assets/style.js
@@ -0,0 +1,29 @@
+function submitButtonClicked() {
+    var userInput = $('#music-input').val();
+
+    if (userInput) {
+        $('#music-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' OCOC2bzZyRBRmVfRUz9H3K3RAOy6t9Fg',
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+function displayGif(response) {
+    $('#musicians').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#musicians').append(image);
+    }
+
